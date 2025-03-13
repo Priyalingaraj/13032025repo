725511736612bbe1241fd110426caaef1969edde
Pooranipriya <pooranipriya.be@gmail.com>
2025-03-13 17:23:51 +0530

    modifiediam user

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "lb_ro" {
 }
 
 resource "aws_iam_user_policy" "lb_ro" {
-  name   = "TESTUSER"
+  name   = "Modifyinguser"
   user   = aws_iam_user.lb.name
   policy = data.aws_iam_policy_document.lb_ro.json
 }
